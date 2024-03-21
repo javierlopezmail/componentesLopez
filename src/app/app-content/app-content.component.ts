557b2842a9fdc64e88a01d8e31fe38358a951d85
Javier <javierlopezmail@gmail.com>
2024-03-20 22:36:47 -0300

    Entrega del primer desafío

---
+++ src/app/app-content/app-content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './app-content.component.html',
+  styleUrl: './app-content.component.scss'
+})
+export class AppContentComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppToolbarComponent } from './app-toolbar/app-toolbar.component';
+import { AppContentComponent } from './app-content/app-content.component';
+import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppToolbarComponent,
+    AppContentComponent,
+    AppSidebarComponent
   ],
   imports: [
     BrowserModule,
